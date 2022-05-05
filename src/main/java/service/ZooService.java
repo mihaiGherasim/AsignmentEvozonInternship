b4d2e7a496ac0feaa7f5f4f8cc4796f3d3d7d513
Mihai <mihaigherasim44@yahoo.com>
2022-05-05 22:49:15 +0300

    Added javadocs in ZooService

--- src/main/java/service/ZooService.java
+++ src/main/java/service/ZooService.java
@@ -12,26 +12,57 @@ public class ZooService {
         this.repository = repository;
     }
 
+    /**
+     * delete an Animal by animal
+     * @param animal
+     * @return true if Animal removed successfully, false otherwise
+     */
     public boolean deleteAnimal(String animal) {
         return repository.deleteAnimal(animal);
     }
 
+    /**
+     * set name @name to an entity Animal identified by animal
+     * @param animal
+     * @param name
+     * @return true if name updated successfully false otherwise
+     */
     public boolean updateNameForAnAnimal(String animal, String name) {
         return repository.updateNameForAnAnimal(animal, name);
     }
 
+    /**
+     * @param animal
+     * @return name for an entity Animal identified by animal
+     */
     public String printNameForAnAnimal(String animal) {
          return repository.printNameForAnAnimal(animal);
     }
 
+    /**
+     * set name for an entity Animal identified by animal if this Animal does not have a name
+     * @param animal
+     * @param name
+     * @return true if name added successfully false otherwise
+     */
     public boolean addNameForAnAnimal(String animal, String name) {
         return repository.addNameForAnimal(animal, name);
     }
 
+    /**
+     * add an entity Animal to file
+     * @param animal
+     * @param name
+     * @return true if Animal added successfully, false otherwise
+     */
     public boolean addAnimal(String animal, String name) {
         return repository.addAnimal(animal, name);
     }
 
+    /**
+     *
+     * @return all Animals from file
+     */
     public List<Animal> getAllAnimals() {
         return repository.getAllAnimals();
     }
